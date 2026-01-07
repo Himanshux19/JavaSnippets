ede8b3166890bde8a55f345236a8b76d55b9b0f9
Himanshu Singh <himanshu124singh19@gmail.com>
2026-01-07 17:01:29 +0530

    Day 6 Commit

---
+++ Java Basics & Logic Building/Day_6.java
@@ -0,0 +1,36 @@
+// Day 6 : Print first N primes
+
+import java.util.*;
+
+public class Day_6 {
+
+    public static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+        if (n <= 3)
+            return true;
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number : ");
+        int n = sc.nextInt();
+        int count = 0;
+        int num = 2;
+        while (count < n) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+                count++;
+            }
+            num++;
+        }
+        sc.close();
+    }
+}
