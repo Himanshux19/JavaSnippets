d5e196bd026d282b72d723f3afe51c237af45420
Himanshu Singh <himanshu124singh19@gmail.com>
2026-02-08 21:50:17 +0530

    Day 37 Commit

---
+++ Searching & Sorting/Day_37.java
@@ -0,0 +1,29 @@
+// Day 37 : Selection Sort
+
+import java.util.*;
+
+public class Day_37 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Size of Array : ");
+        int n = sc.nextInt();
+        System.out.println("Enter the Array Elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min])
+                    min = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+        System.out.println("Sorted Array");
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+        sc.close();
+    }
+}
