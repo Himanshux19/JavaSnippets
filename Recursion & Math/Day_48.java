1a57153123df715f72619d8d4f5925bc74b34dfa
Himanshu Singh <himanshu124singh19@gmail.com>
2026-02-19 23:50:28 +0530

    Day 48 Commit

---
+++ Recursion & Math/Day_48.java
@@ -0,0 +1,22 @@
+// Day 47 : Tower of Hanoi
+
+import java.util.*;
+
+public class Day_48 {
+
+    public static void toh(int n, char src, char aux, char dest) {
+        if (n == 0)
+            return;
+        toh(n - 1, src, dest, aux);
+        System.out.println(src + " -> " + dest);
+        toh(n - 1, aux, src, dest);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number of Disk : ");
+        int n = sc.nextInt();
+        toh(n, 'A', 'B', 'C');
+        sc.close();
+    }
+}
