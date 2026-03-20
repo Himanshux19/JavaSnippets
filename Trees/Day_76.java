d12bb574bd1e791db7bc3ec6c485f6abbabf53b8
Himanshu Singh <himanshu124singh19@gmail.com>
2026-03-20 14:30:49 +0530

    Day 76 Commit

---
+++ Trees/Day_76.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Day_76 {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    public static int height(Node root) {
+        if (root == null)
+            return 0;
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
+
+    public static Node buildTree() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter node value (-1 for NULL): ");
+        int val = sc.nextInt();
+        if (val == -1)
+            return null;
+        Node root = new Node(val);
+        System.out.println("Enter left child of " + val);
+        root.left = buildTree();
+        System.out.println("Enter right child of " + val);
+        root.right = buildTree();
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Node root = buildTree();
+        int h = height(root);
+        System.out.println("Height of thr Tree is : " + h);
+    }
+}
