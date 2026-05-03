041a99ee48c594a53169cffaef1b79f049eefa4b
Himanshu Singh <himanshu124singh19@gmail.com>
2026-05-03 21:43:06 +0530

    Day 103 Commit

---
+++ Advanced Arrays & Sliding Window/Day_103.java
@@ -0,0 +1,27 @@
+// Day 103 : Longest Repeating Character Replacement
+
+import java.util.Scanner;
+
+class Day_103 {
+    public static int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int left = 0, maxFreq = 0, maxLen = 0;
+        for (int right = 0; right < s.length(); right++) {
+            freq[s.charAt(right) - 'A']++;
+            maxFreq = Math.max(maxFreq, freq[s.charAt(right) - 'A']);
+            while ((right - left + 1) - maxFreq > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int k = sc.nextInt();
+        System.out.println("Minimum size Subarray sum : " + characterReplacement(s, k));
+    }
+}
