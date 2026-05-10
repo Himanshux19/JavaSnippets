4536dcd9a02ebd0b621e3aa39ca5d2e94034228b
Himanshu Singh <himanshu124singh19@gmail.com>
2026-05-10 18:57:30 +0530

    Day 115 Commit

---
+++ Two Pointers & Greedy/Day_115.java
@@ -0,0 +1,24 @@
+// Day 115 : Remove duplicates (unsorted)
+
+import java.util.*;
+
+class Day_115 {
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num))
+                return true;
+            set.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        System.out.println(containsDuplicate(arr) ? "Contains Duplicate" : "Didn't Contains Dupllicate");
+    }
+}
